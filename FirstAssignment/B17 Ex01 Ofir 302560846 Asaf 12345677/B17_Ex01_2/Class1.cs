399c04ea0d2016e0b1e2b88e3c6e783bc86255d8
DESKTOP-T5UJ4HK\ofiriro3 <ofiriro3@gmail.com>
2017-05-16 10:00:35 +0300

    new project was made with the all infrastructure

---
+++ FirstAssignment/B17 Ex01 Ofir 302560846 Asaf 12345677/B17_Ex01_2/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B17_Ex01_2
+{
+    class Class1
+    {
+    }
+}
---
+++ FirstAssignment/B17 Ex01 Ofir 302560846 Asaf 12345677/Project1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B17_Ex01_1 
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
