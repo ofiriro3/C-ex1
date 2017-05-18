97cdeb5b97f25d9dc68493ec4aa91471e14b87df
DESKTOP-T5UJ4HK\ofiriro3 <ofiriro3@gmail.com>
2017-05-18 10:50:43 +0300

    Final merge was made

--- FirstAssignment/B17 Ex01 Ofir 302560846 Asaf 12345677/B17_Ex01_2/Class1.cs
+++ FirstAssignment/B17 Ex01 Ofir 302560846 Asaf 12345677/B17_Ex01_2/Class1.cs
@@ -2,13 +2,37 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace B17_Ex01_2
 {
     class Program
     {
         public static void Main()
         {
+            string hourglass = beginnersHourglass();
 
+            Console.WriteLine(hourglass);
+            Console.WriteLine("Please press 'Enter' to exit");
+            Console.ReadLine();
+        }
+
+        public static string beginnersHourglass()
+        {
+            string levelOne = "******";
+            string levelTwo = " *** ";
+            string levelThree = "  *  ";
+            string levelFour = " *** ";
+            string levelFive = "******";
+            string hourglass = string.Format(
+                @"
+                {4}
+                {3}
+                {2}
+                {1}
+                {0}", levelOne, levelTwo, levelThree, levelFour, levelFive);
+
+            return hourglass;
         }
     }
 }
+
