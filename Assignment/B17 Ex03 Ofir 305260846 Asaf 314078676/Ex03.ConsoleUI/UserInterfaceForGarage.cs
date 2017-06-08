00ccc9de577408ccba8e2f8fd8d2b88adeca8cbe
DESKTOP-T5UJ4HK\ofiriro3 <ofiriro3@gmail.com>
2017-06-08 22:06:42 +0300

    ofir's commit

--- Assignment/B17 Ex03 Ofir 305260846 Asaf 314078676/Ex03.ConsoleUI/UserInterfaceForGarage.cs
+++ Assignment/B17 Ex03 Ofir 305260846 Asaf 314078676/Ex03.ConsoleUI/UserInterfaceForGarage.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Ex03.GarageLogic;
+using Garage = Ex03.GarageLogic.Garage;
+using SystemVehicleManger = Ex03.GarageLogic.SystemVehicleManger;
 
 namespace Ex03.ConsoleUI
 {
@@ -11,6 +14,8 @@ namespace Ex03.ConsoleUI
 
         public static void run()
         {
+            Garage myGarage = new Garage();
+
             bool programIsRunning = true;
             while (programIsRunning)
             {
@@ -18,33 +23,182 @@ namespace Ex03.ConsoleUI
 (@"Please choose one of the folowing
 Press 1 for add another Vehicle to the garage
 Press 2 for print the License Plates of all cars that are now in the garage
-Press 3 for print the License Plates of all cars that are now in the garage with specific status
-Press 4 for change a car status in the garage
-Press 5 to inflate one of the cars wheels
-Press 6 to fuel a car
-Press 7 to charge a battery of an electricity car
-Press 8 for full details of a specific car"));
+Press 3 for change a car status in the garage
+Press 4 to inflate one of the cars wheels
+Press 5 to fuel a car
+Press 6 to charge a battery of an electricity car
+Press 7 for full details of a specific car"));
 
                 int inputCommand;
                 getValidInputFromUser(out inputCommand);
                 switch (inputCommand)
                 {
                     case -1: programIsRunning = false; break;
-                    case 1:AddNewCarToGarage();break;
-                    case 2: AddNewCarToGarage(); break;
-                    case 3: AddNewCarToGarage(); break;
-                    case 4: AddNewCarToGarage(); break;
-                    case 5: AddNewCarToGarage(); break;
-                    case 6: AddNewCarToGarage(); break;
-                    case 7: AddNewCarToGarage(); break;
-                    case 8: AddNewCarToGarage(); break;
-                    default: Console.WriteLine("Invalid input , please choose a number between 0 - 8");
+                    case 1: addNewCarToGarage(myGarage);break;
+                    case 2: printCarList(myGarage); break;
+                    case 3: changeCarStatus(myGarage); break;
+                    case 4: inflateCarWheels(myGarage); break;
+                    case 5: toFuelACar(myGarage); break;
+                    case 6: toChargeAnElectricCar(myGarage); break;
+                    case 7: printFullDetailsOfACar(myGarage); break;
+                    default: Console.WriteLine("Invalid input , please choose a number between 0 - 7 "); break;
                 }
 
 
             }
         }
 
+        private static void printFullDetailsOfACar(Garage io_Garage)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void toChargeAnElectricCar(Garage io_Garage)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void toFuelACar(Garage io_Garage)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void inflateCarWheels(Garage io_Garage)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void changeCarStatus(Garage io_Garage)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void printCarList(Garage io_Garage)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void addNewCarToGarage(Garage io_Garage)
+        {
+            Console.WriteLine("Please enter your car ID");
+            string carID = Console.ReadLine();
+            bool exist = SystemVehicleManger.FindCarByLicensePlate(io_Garage, carID);
+            if(exist)
+            {
+                Console.WriteLine(string.Format("Your car is already in the garage {0}", carID));
+            }
+            else
+            {
+                creatNewVehicle(carID, io_Garage);
+            }
+            
+
+
+        }
+
+        private static void creatNewVehicle(string carID,Garage io_Garage)
+        {
+            Console.WriteLine(string.Format
+(@"Please choose the car type that you want to enter the garage
+1. Regular motorcycle
+2. Electircal motorcycle
+3. Regular car
+4. Truck
+or press Q to go back"));
+
+            int commandToDo;
+            getValidInputFromUser(out commandToDo);
+            switch (commandToDo)
+            {
+                case -1: ; break;
+                case 1: createRegularMotorCycle(io_Garage, carID); break;
+                case 2: createElectornicMotorCycle(io_Garage, carID); break;
+                case 3: createRegularCar(io_Garage, carID); break;
+                case 4: createTruck(io_Garage, carID); break;
+                case 7: printFullDetailsOfACar(io_Garage, carID); break;
+                default: Console.WriteLine("Invalid input , please choose a number between 0 - 7 "); break;
+            }
+
+        }
+        public static List<float> getElectricalDetails()
+        {
+            List<float> electricDetails = new List<float>();
+            Console.WriteLine("Please enter how much power left in your battery");
+            string powerLeft = Console.ReadLine();
+            bool validPower = false;
+            while(validPower)
+            {
+                float powerLeftNumber;
+                bool isANumber = float.TryParse(powerLeft, out powerLeftNumber);
+                if(powerLeftNumber <= 100 && powerLeftNumber >= 0)
+                {
+                    electricDetails.Add(powerLeftNumber);
+                    break;
+                }
+            }
+
+            return electricDetails;
+        }
+
+        public static List<float> GetVeichelByFuelDetails()
+        {
+            List<float> FuelDetailsList = new List<float>();
+            Console.WriteLine("Please eneter how much fuel do you have in your tank");
+            
+            bool isNumber = false;
+            while (! isNumber)
+            {
+                string inputFuelFromUser = Console.ReadLine();
+                float fuelLeftInTank;
+                bool isANumber = float.TryParse(inputFuelFromUser, out fuelLeftInTank);
+            }
+
+            return FuelDetailsList;
+            
+
+        }
+        private static List<String> getGenralDeatailsVehicle (string carID)
+        {
+            List<String> details = new List<string>();
+            details.Add(carID);
+            Console.WriteLine("We would like you to write few necessary details for our mangment System");
+            Console.WriteLine("Please enter the Vehicle module");
+            string carModule = Console.ReadLine();
+            details.Add(carID);
+            Console.WriteLine("Please enter the Vehicle License Plate");
+            string vehicleLicense = Console.ReadLine();
+            details.Add(vehicleLicense);
+            Console.WriteLine("Please enter the Name of the owner");
+            string owner = Console.ReadLine();
+            details.Add(owner);
+            Console.WriteLine("Please enter your Cellphone");
+            string CellPhonenumber = Console.ReadLine();
+            details.Add(CellPhonenumber);
+
+            return details;
+
+        }
+
+        private static List<String> getWeelsDetails()
+        {
+            List<String> details = new List<string>();
+            Console.WriteLine("We would like you to write few necessary details for our mangment System");
+            Console.WriteLine("Please enter the Vehicle module");
+            string carModule = Console.ReadLine();
+            details.Add(carID);
+            Console.WriteLine("Please enter the Vehicle License Plate");
+            string vehicleLicense = Console.ReadLine();
+            details.Add(vehicleLicense);
+            Console.WriteLine("Please enter the Name of the owner");
+            string owner = Console.ReadLine();
+            details.Add(owner);
+            Console.WriteLine("Please enter your Cellphone");
+            string CellPhonenumber = Console.ReadLine();
+            details.Add(CellPhonenumber);
+
+            return details;
+
+        }
         private static void getValidInputFromUser(out int o_commandForTheComputerToexecute)
         {
                 bool validCommandFromUser = false;
@@ -58,8 +212,12 @@ Press 8 for full details of a specific car"));
                     {
                         o_commandForTheComputerToexecute = -1;
                     }
-
-                    validCommandFromUser = int.TryParse(inputFromUser, out o_commandForTheComputerToexecute);               
+                    validCommandFromUser = int.TryParse(inputFromUser, out o_commandForTheComputerToexecute);         
+                    
+                    if( validCommandFromUser == false)
+                    {
+                    Console.WriteLine("Wrong input format please type again");
+                    }
                 }
         }
 
--- Assignment/B17 Ex03 Ofir 305260846 Asaf 314078676/Ex03.GarageLogic/SystemVehicleManger.cs
+++ Assignment/B17 Ex03 Ofir 305260846 Asaf 314078676/Ex03.GarageLogic/SystemVehicleManger.cs
@@ -4,8 +4,25 @@ using System.Text;
 
 namespace Ex03.GarageLogic
 {
-    class SystemVehicleManger
+    public static class SystemVehicleManger
     {
 
+
+
+        public static bool FindCarByLicensePlate(Garage io_Garage,string i_id)
+        {
+            bool isExist = false;
+            foreach (string id in io_Garage.GetListOfLicensePlateNumbersOfVehiclesInGarage())
+            {
+                if(id == i_id)
+                {
+                    io_Garage.ChangeStateOfVehicle(i_id, Garage.GarageVehicle.eVehicleStatus.InRepair);
+                    isExist = true;
+                    break;
+                }
+            }
+
+            return isExist;
+        }
     }
 }
